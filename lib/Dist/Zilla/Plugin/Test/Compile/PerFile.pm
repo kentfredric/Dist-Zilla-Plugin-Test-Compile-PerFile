3600e282d5a793d32bb1168352cb10e5fc65f6c8
Kent Fredric <kentfredric@gmail.com>
2017-03-01 13:43:01 +1300

    Build results of a7c5182 (on master)

--- lib/Dist/Zilla/Plugin/Test/Compile/PerFile.pm
+++ lib/Dist/Zilla/Plugin/Test/Compile/PerFile.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::Plugin::Test::Compile::PerFile;
 
-our $VERSION = '0.003902';
+our $VERSION = '0.003903';
 
 # ABSTRACT: Create a single .t for each compilable file in a distribution
 
@@ -502,7 +502,7 @@ Dist::Zilla::Plugin::Test::Compile::PerFile - Create a single .t for each compil
 
 =head1 VERSION
 
-version 0.003902
+version 0.003903
 
 =head1 SYNOPSIS
 
